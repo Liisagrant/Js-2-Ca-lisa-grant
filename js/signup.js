96d7457d717716cf2ddf103c158d9efb0033d5d1
Liisagrant <lisagrant1990@gmail.com>
2022-10-06 15:22:14 +0200

    changed the index page to login and signup

---
+++ js/signup.js
@@ -0,0 +1,12 @@
+import { SIGN_UP_URL } from "./settings/api";
+
+const signUpForm = document.querySelector("#signup-form");
+
+const firstName = document.querySelector("#firstname");
+console.log(firstName);
+const email = document.querySelector("#email");
+console.log(email);
+const password = document.querySelector("#password");
+console.log(password);
+const confirmPassword = document.querySelector("#confirmpassword");
+console.log(confirmPassword);
