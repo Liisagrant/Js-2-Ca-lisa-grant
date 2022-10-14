4e0cb6157ccce14b68eb4747b4eb8fcf8492fc65
Liisagrant <lisagrant1990@gmail.com>
2022-10-14 15:48:08 +0200

    fixed styling issues in navbar

--- js/components/nav.js
+++ js/components/nav.js
@@ -7,40 +7,40 @@ const myNavBar = () => {
     const userName = getUserName();
     let navBarLinks;
     navBarLinks = `
-            <a  class="px-2.5 py-2" href="#">
+            <a  class="px-2.5 py-6" href="#">
               <span class="text-white text-3xl font-bold">PALS</span>
             </a>
-            <li class="px-4 py-4">
+            <li class="px-4 py-7 text-white">
             <a href ="/index.html" class="${
-              pathname === "/index.html" ? "text-white" : ""
+              pathname === "/index.html" ? "text-gray-700" : ""
             }">LogIn</a>
             </li>
-            <li class="px-4 py-4"><a href ="/signup.html" class="${
-              pathname === "/signup.html" ? "text-white" : ""
+            <li class="px-4 py-7 text-white"><a href ="/signup.html" class="${
+              pathname === "/signup.html" ? "text-gray-700" : ""
             }">SignUp</a></li>
 
       `;
     if (userName) {
       navBarLinks = `
-            <a class="px-1 py-4" href="/homepage.html">
-              <span class="text-white px-4 py-8 text-2xl font-bold">PALS</span>
+            <a class="px-1 py-5" href="/homepage.html">
+              <span class="text-white px-1 py-5 text-lg sm:text-3xl sm:py-8 font-bold">PALS</span>
             </a>
-            <li class="px-2 py-6">
+            <li class="px-1.5 py-6 text-white sm:px-4 sm:py-7">
             <a href="/homepage.html" class="${
-              pathname === "/homepage.html" ? "text-white" : ""
-            }">🏠</a>
+              pathname === "/homepage.html" ? "text-gray-700" : ""
+            }">Home</a>
             </li>
-            <li class="px-2 py-6 ">
+            <li class="px-1.5 py-6 text-white sm:px-4 sm:py-7">
             <a href ="/my-posts.html" class="${
-              pathname === "/my-posts.html" ? "text-white" : ""
+              pathname === "/my-posts.html" ? "text-gray-700" : ""
             }">MyPosts</a>
             </li>
-            <li class="px-2 py-6 ">
+            <li class="px-1.5 py-6 text-white sm:px-4 sm:py-7">
             <a href ="/profile.html" class="${
-              pathname === "/profile.html" ? "text-white" : ""
-            }">👤</a>
+              pathname === "/profile.html" ? "text-gray-700" : ""
+            }">Profile</a>
             </li>
-            <li class="px-2 py-6"><button id="logoutbtn">LogOut</button></li>
+            <li class="px-1.5 py-6 sm:px-4 sm:py-7"><button  class="text-white" id="logoutbtn">LogOut</button></li>
           
     
           `;
