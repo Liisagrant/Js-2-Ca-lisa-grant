58850b7c07c5cf7e0d378a8e2687011bcd045571
Liisagrant <lisagrant1990@gmail.com>
2022-10-17 13:49:20 +0200

    cleaning up code

--- js/profile.js
+++ js/profile.js
@@ -1,14 +1,40 @@
-import {getToken} from "./utils/storage";
-import {GET_PROFILE} from "./settings/api";
-import {isMoment} from "moment";
+import { getToken } from "./utils/storage";
+import { GET_USER_POSTS } from "./settings/api";
 import moment from "moment";
 
 const accessToken = getToken();
 if (!accessToken) {
-    location.href = "/index.html";
+  location.href = "/index.html";
 }
 
 const profileContainer = document.querySelector("#profile-info");
-console.log(profileContainer);
-
 let now = moment(new Date());
+
+const getProfileInfo = async () => {
+  const response = await fetch(GET_USER_POSTS, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    profileContainer.innerHTML = `
+        <div class=" bg-slate-200 px-6 py-6 rounded-md">
+        <div>
+            <h2 class="font-bold py-2">User Name: ${data.name}</h2>
+            <p class="font-medium py-1">User email: ${data.email}</p>
+          </div>
+          <div class="flex justify-center">
+            <img src="./img/profile-default-img.jpg" class="w-36 rounded-full mt-6" />
+          </div>
+        </div>
+
+        
+        `;
+  } else {
+    console.log(error);
+  }
+};
+
+getProfileInfo();
